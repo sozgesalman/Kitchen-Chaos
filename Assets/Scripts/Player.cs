fbf79c925729bac4fa133c828f9938d2a1bbb492
özge salman <49956992+sozgesalman@users.noreply.github.com>
2025-01-02 23:29:47 +0300

    feat: add rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,35 +6,47 @@ public class Player : MonoBehaviour
 {
     private Vector3 _position;
     [SerializeField] private float _speed;
+    private bool isWalking;
+
+    public bool IsWalking { get { return isWalking; }     
+    }
+
     private void Start()
-    {
-        _position = transform.position;
-       
+    {               
         Application.targetFrameRate = 60;
     }
 
 
 
     private void Update()
-    {        
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
         if (Input.GetKey(KeyCode.W))
         {
-            _position.z = +1;
+            inputVector.y = +1;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            _position.x = -1;
+            inputVector.x = -1;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            _position.z = -1;
+            inputVector.y = -1;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            _position.x = +1;
+            inputVector.x = +1;
         }
-        _position = _position.normalized;
+
+        inputVector = inputVector.normalized;
+        _position = new Vector3(inputVector.x, 0, inputVector.y);
+
         transform.position += _position * _speed * Time.deltaTime;
+
+        isWalking = _position != Vector3.zero;
+
+        transform.forward = Vector3.Slerp(transform.forward, _position, Time.deltaTime * _speed);
         print(transform.position);
 
     }
